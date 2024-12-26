7ffef896646ad08e42459a1680d21e5b9d86f16b
Egnaldo Mello <egnaldomello@gmail.com>
2024-12-26 18:45:52 -0300

    6º commit - criado o slide no javascript e adicionado os efeitos nos icones 1 e 2 ao scrollar

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
+/*efeito com scroll*/
 const target = document.querySelectorAll('[data-anime]');
+const icone1 = document.querySelector('.icone1');
+const icone2 = document.querySelector('.icone2');
+
+console.log(icone2.offsetTop)
 
 function scroll() {
     const topoWindow = window.scrollY + ((window.innerHeight * 3) / 4);
+    if(topoWindow > Math.abs(icone1.offsetTop) * 20) { 
+        icone1.classList.add('girar')
+        } else {
+        icone1.classList.remove('girar')
+        };
+    if(topoWindow > icone2.offsetTop * 85) { 
+        icone2.classList.add('saltar')
+        } else {
+        icone2.classList.remove('saltar')
+        };
     target.forEach((e) => {
         if(topoWindow > e.offsetTop) {
             e.classList.add('animate')
         } else {
             e.classList.remove('animate')
-        }
+        }   
     })
 }
 
@@ -17,3 +32,40 @@ if(target.length){
 window.addEventListener('scroll', function(){
     scroll();
 })};
+
+/*slide*/
+const imagens = document.querySelectorAll('.slide');
+const setad = document.querySelector('.setad');
+const setae = document.querySelector('.setae');
+let i = 0;
+
+function mostrarimg() {
+    imagens[i].classList.add('frente');
+}
+
+function esconderimg() {
+    imagens.forEach(e => e.classList.remove('frente'))
+}
+
+function imganterior() {
+    esconderimg()
+    if(i == 0) {
+        i = imagens.length - 1;    
+    } else {
+        i--
+    }
+    mostrarimg()
+}
+
+function imgposterior() {
+    esconderimg()
+    if(i == imagens.length - 1) {
+        i = 0    
+    } else {
+        i++
+    }
+    mostrarimg()
+}
+
+setad.addEventListener('click', imgposterior)
+setae.addEventListener('click', imganterior)
